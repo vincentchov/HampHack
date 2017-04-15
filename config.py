e85da0d2c36ec5e7eab8ecb2a4e01e9bec417dec
John Bojorquez <john.bojorquez@uconn.edu>
2017-04-14 22:19:56 -0400

    Configure mysql ORM and migrations

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os, logging
+from db_config import *
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/' + DB_NAME
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')

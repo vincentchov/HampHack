c6255a1aad85c8311f80c2251ba50965b018589a
Julie Pham <julie.pham@uconn.edu>
2017-04-15 02:31:02 -0400

    Modify map

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,8 +1,9 @@
 function initMap() {
     var hampshireCollege = {lat: 42.3241, lng: -72.5307};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: hampshireCollege
+      zoom: 15,
+      center: hampshireCollege,
+      disableDefaultUI: true
     });
     var marker = new google.maps.Marker({
       position: hampshireCollege,

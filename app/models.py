353254adba5629e03e83d6b544e19e370584383f
John Bojorquez <john.bojorquez@uconn.edu>
2017-04-15 00:21:58 -0400

    Create model and migratiton for pins

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    pins = db.relationship('Pin', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.email)
+
+class Pin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description = db.Column(db.Text)

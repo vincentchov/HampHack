3f525c2f3eabd4a055b63fcf1c81b3dcf4e8fff4
Vincent Chov <vincentchov@gmail.com>
2017-04-15 16:58:40 -0400

    Profit

---
+++ db_config.py
@@ -0,0 +1,4 @@
+DB_HOST='breadcrumbs.caosexgo1ndr.us-east-1.rds.amazonaws.com:3306'
+DB_NAME='breadcrumbs'
+DB_USER='vincenzo'
+DB_PASS='JaWnNyDOrsqJWCgTXhXz'
---
+++ secrets.py
@@ -0,0 +1,3 @@
+SECRET_GOOGLE_CLIENT_ID = "439257213003-dgtflmnplbn41i20pijcnbrkov26r9nf.apps.googleusercontent.com"
+SECRET_GOOGLE_CLIENT_SECRET = "wRUFoCUvdY-6U3gYw5C2yP4E"
+MY_SECRET_KEY = "you-will-never-guess"

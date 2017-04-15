615c4b1c5908d9b97214930ae18520a7d14a0598
John Bojorquez <john.bojorquez@uconn.edu>
2017-04-14 22:20:27 -0400

    Add User model and migration

--- app/models.py
+++ app/models.py
@@ -1,1 +1,8 @@
-from app import app
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.email)

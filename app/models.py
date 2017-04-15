e64e0b78dfe647fe13e8901c33f0588d075d92cd
John Bojorquez <john.bojorquez@uconn.edu>
2017-04-15 02:18:01 -0400

    Add routes for creating and deleting pins

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     pins = db.relationship('Pin', backref='user', lazy='dynamic')
 
+    def __init__(self, email):
+        self.email = email
+
     def __repr__(self):
         return '<User %r>' % (self.email)
 
@@ -13,3 +16,8 @@ class Pin(db.Model):
     name = db.Column(db.String(120), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     description = db.Column(db.Text)
+
+    def __init__(self, name, user_id, description):
+        self.name = name
+        self.user_id = user_id
+        self.description = description

b5222707571a9112d90358c0925103aa23cfe382
Vincent Chov <vincentchov@gmail.com>
2017-04-14 22:31:45 -0400

    Proper config for Google OAuth

--- config.py
+++ config.py
@@ -11,16 +11,18 @@ oauth = OAuth()
 # https://code.google.com/apis/console
 GOOGLE_CLIENT_ID = SECRET_GOOGLE_CLIENT_ID
 GOOGLE_CLIENT_SECRET = SECRET_GOOGLE_CLIENT_SECRET
-REDIRECT_URI = '/oauth-authorized'  # one of the Redirect URIs from Google APIs console
+REDIRECT_URI = '/authorized'  # one of the Redirect URIs from Google APIs console
 
-google = oauth.remote_app('google',
-                          base_url='https://www.google.com/accounts/',
-                          authorize_url='https://accounts.google.com/o/oauth2/auth',
-                          request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-                                                'response_type': 'code'},
-                          access_token_url='https://accounts.google.com/o/oauth2/token',
-                          access_token_method='POST',
-                          access_token_params={'grant_type': 'authorization_code'},
-                          consumer_key=GOOGLE_CLIENT_ID,
-                          consumer_secret=GOOGLE_CLIENT_SECRET)
+google = oauth.remote_app(
+    'google',
+    consumer_key=GOOGLE_CLIENT_ID,
+    consumer_secret=GOOGLE_CLIENT_SECRET,
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
